66a96ae49a5789d3e41507e0d69ab014797539a6
Precious Bless Lumabas Florentino <129078667+Precious143Bless@users.noreply.github.com>
2026-05-01 22:20:34 +0800

    wsgi

--- veggiematch/veggiematch/wsgi.py
+++ veggiematch/veggiematch/wsgi.py
@@ -1,5 +1,27 @@
+# WSGI configuration for VeggieMatch
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veggiematch.settings')
+# IMPORTANT: Point to the inner folder where manage.py is
+path = '/home/PreciousBless/VeggieMatch/veggiematch'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Add parent directory to path as well (for imports)
+parent_path = '/home/PreciousBless/VeggieMatch'
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+
+# Set Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'veggiematch.settings'
+
+# Activate virtual environment
+activate_this = '/home/PreciousBless/.virtualenvs/veggiematch-env/bin/activate_this.py'
+try:
+    with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
+except FileNotFoundError:
+    pass
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
