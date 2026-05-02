75ed3ddebf5a22449aff0a859ddb077dc0de2968
Precious Bless Lumabas Florentino <florentinobless123@gmail.com>
2026-05-02 09:16:31 +0800

    update

--- veggiematch/veggiematch/__init__.py
+++
@@ -1,7 +0,0 @@
-# Required only if you installed PyMySQL instead of mysqlclient.
-# If you installed mysqlclient, you can leave this file empty.
-try:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    pass
